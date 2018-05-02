6cff91cc9548d95b4cb90bcc543d0b78028e1a14
Unknown <cgc0045@alu.ubu.es>
2018-05-02 17:38:18 +0200

    Animacion Inicio III
    Animacion de inicio con un diseño mas sofisticado, intentando mantener el diseño de la UBU

--- Codigo Fuente/Android/UBUAssistant/app/src/main/java/es/ubu/cgc0045/ubuassistant/SplashScreen.java
+++ Codigo Fuente/Android/UBUAssistant/app/src/main/java/es/ubu/cgc0045/ubuassistant/SplashScreen.java
@@ -30,13 +30,13 @@ public class SplashScreen extends Activity{
         //Create the animation and start it
         Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
         anim.reset();
-        LinearLayout l = (LinearLayout) findViewById(R.id.lin_lay);
+        LinearLayout l = findViewById(R.id.lin_lay);
         l.clearAnimation();
         l.startAnimation(anim);
 
         anim = AnimationUtils.loadAnimation(this, R.anim.translate);
         anim.reset();
-        ImageView iv = (ImageView) findViewById(R.id.logo);
+        ImageView iv = findViewById(R.id.logo);
         iv.clearAnimation();
         iv.startAnimation(anim);
 
